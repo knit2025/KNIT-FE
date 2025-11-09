b129affafce8df0b9bfd040c414fa2f8f05183fd
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-09 11:56:55 +0900

    로그인 페이지 기본 레이아웃 및 컴포넌트 구성

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import InputField from "../../components/Login/InputField";
+import Button from "../../components/Login/Button";
+// import sheepImg from "../../assets/LoginSheep.png";
 
 const Login: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <span>가족 사이의 마음과 마음을 잇는</span>
+        <span>KNIT</span>
+      </div>
+      <InputField feildName="아이디 입력"></InputField>
+      <InputField feildName="비밀번호 입력"></InputField>
+      <Button buttonName="로그인"></Button>
+      <Button buttonName="회원가입"></Button>
+    </div>
+  );
 };
 
 export default Login;
