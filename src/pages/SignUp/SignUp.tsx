c1d696a7be252e02b7f53098bd2232051eb03dab
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 03:56:51 +0900

    회원가입 페이지 css 적용

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import SignupInput from "../../components/SignUp/SignupInput";
-import Button from "../../components/Login/Button";
+import SignupBtn from "../../components/SignUp/SignupBtn";
 
 const SignUp: React.FC = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <SignupInput
-            fieldName="아이디"
-            type="email"
-            placeholder="아이디 입력"
-          />
-          <button>중복 확인</button>
+    <div className="w-[390px] h-screen flex flex-col items-center bg-white">
+      <h1 className="text-[#3A290D] font-sans text-[1.5rem] font-semibold pt-20 pb-10">
+        회원가입
+      </h1>
+      <div className="flex flex-col gap-4 mt-6 m-5">
+        <div className="flex flex-col text-left">
+          <label className="font-sans text-[#3A290D] font-semibold text-[0.875rem] pb-4">
+            아이디
+          </label>
+          <div className="flex gap-2">
+            <input
+              className="w-[16.1875rem] h-5 bg-white focus:outline-none focus:ring-0 placeholder:text-[#CBCBCB] placeholder: text-[0.75rem] text-[#786B56] border-b border-[#DDD] pb-2"
+              type="text"
+              placeholder="아이디 입력"
+            ></input>
+            <button className="w-15.25 h-6 rounded-lg bg-[#523E1B] text-white font-sans text-[0.6875rem] cursor-pointer">
+              중복 확인
+            </button>
+          </div>
         </div>
         <SignupInput
           fieldName="비밀번호"
@@ -30,11 +40,15 @@ const SignUp: React.FC = () => {
           type="text"
           placeholder="8자리 입력"
         />
-        <div>
-          <p>*내가 우리 가족의 첫 가입자에요</p>
-          <button>가족 코드 생성</button>
+        <div className="flex gap-23">
+          <p className="text-[#3A290D] font-sans text-[0.75rem] font-semibold">
+            *내가 우리 가족의 첫 가입자에요
+          </p>
+          <button className="w-20 h-6 rounded-lg bg-[#523E1B] text-white font-sans text-[0.6875rem] cursor-pointer">
+            가족 코드 생성
+          </button>
         </div>
-        <Button buttonName="회원가입" />
+        <SignupBtn buttonName="회원가입" />
       </div>
     </div>
   );
