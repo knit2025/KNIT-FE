dbcec319b5e421e65566715590ff794c82e7fd1c
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-10 23:31:27 +0900

    Button.tsx css 적용

--- src/components/Login/Button.tsx
+++ src/components/Login/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/Global.css";
 
 interface ButtonProps {
   buttonName: string;
@@ -6,9 +7,14 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ buttonName }) => {
   return (
-    <>
-      <div>{buttonName}</div>
-    </>
+    <div>
+      <button
+        type="submit"
+        className="w-81 h-11.75 rounded-[1.4375rem] bg-[#F7F7F7] text-[#3A290D] font-suit font-weight-500 font-size-1.125rem cursor-pointer"
+      >
+        {buttonName}
+      </button>
+    </div>
   );
 };
 
