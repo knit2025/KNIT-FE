2bcf8585e05439ec2d4162fc8aabb9c221cc4837
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 04:04:15 +0900

    가족 코드 페이지 레이아웃 기본 설정

---
+++ src/pages/FamilyCode/FamilyCode.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FamilyCode: React.FC = () => {
+  return (
+    <div className="w-[390px] h-screen flex flex-col items-center bg-[#D7BCA8]">
+      <div>
+        <p>Family code</p>
+        <div>
+          <p>F3RT0Y</p>
+        </div>
+        <p>가족과 연결하려면 코드가 필요해요! 가족에게 코드를 공유해주세요.</p>
+      </div>
+    </div>
+  );
+};
+
+export default FamilyCode;
