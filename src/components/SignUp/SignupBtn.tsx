ea65e3d81fe8d59dcd4b3a57314359f73799b22e
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 03:57:02 +0900

    회원가입 버튼 컴포넌트화

---
+++ src/components/SignUp/SignupBtn.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/Global.css";
+
+interface ButtonProps {
+  buttonName: string;
+}
+
+const SignupBtn: React.FC<ButtonProps> = ({ buttonName }) => {
+  return (
+    <div className="pt-20 pb-20">
+      <button
+        type="submit"
+        className="w-81 h-11.75 rounded-[1.4375rem] bg-[#A9927F] text-[#F3F0ED] font-suit font-weight-500 font-size-1.125rem cursor-pointer"
+      >
+        {buttonName}
+      </button>
+    </div>
+  );
+};
+
+export default SignupBtn;
