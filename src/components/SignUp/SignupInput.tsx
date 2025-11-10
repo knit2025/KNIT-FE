0d063db4278b702608780470d8e621a95d524f04
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 03:56:39 +0900

    회원가입 input css 적용

--- src/components/SignUp/SignupInput.tsx
+++ src/components/SignUp/SignupInput.tsx
@@ -12,12 +12,12 @@ const SignupInput: React.FC<FieldProps> = ({
   placeholder,
 }) => {
   return (
-    <div>
-      <label className="font-sans text-[#3A290D] font-semibold">
+    <div className="flex flex-col text-left">
+      <label className="font-sans text-[#3A290D] font-semibold text-[0.875rem] pb-5">
         {fieldName}
       </label>
       <input
-        className="w-[20.18763rem] h-px border-[red]"
+        className="w-[20.18763rem] h-5 bg-white focus:outline-none focus:ring-0 placeholder:text-[#CBCBCB] placeholder: text-[0.75rem] text-[#786B56] border-b border-[#DDD] pb-2"
         type={type}
         placeholder={placeholder}
       ></input>
