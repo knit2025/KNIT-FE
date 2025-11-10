02322f7e9fb25d762cf0a57d304de9b7af085844
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-10 23:31:13 +0900

    InputField.tsx css 적용

--- src/components/Login/InputField.tsx
+++ src/components/Login/InputField.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-interface FeildNameProps {
-  feildName: string;
+interface FieldNameProps {
+  fieldName: string;
+  type: string;
 }
 
-const InputField: React.FC<FeildNameProps> = ({ feildName }) => {
+const InputField: React.FC<FieldNameProps> = ({ fieldName, type }) => {
   return (
-    <>
-      <input placeholder={feildName}></input>
-    </>
+    <div>
+      <input
+        type={type}
+        className="w-81 h-9.75 bg-[#F7F7F7] rounded-[0.9375rem] focus:outline-none focus:ring-0 placeholder:text-[#786B56] placeholder: text-[0.875rem] text-[#786B56] pl-4"
+        placeholder={fieldName}
+      ></input>
+    </div>
   );
 };
 
