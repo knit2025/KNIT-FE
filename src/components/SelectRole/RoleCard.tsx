82020d28f7c5c29c33c72eb08268cd58ffa51e41
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 11:50:42 +0900

    역할 카드 padding 값 조정

--- src/components/SelectRole/RoleCard.tsx
+++ src/components/SelectRole/RoleCard.tsx
@@ -6,8 +6,8 @@ interface roleProps {
 
 const RoleCard: React.FC<roleProps> = ({ role }) => {
   return (
-    <div className="flex h-7.75 text-center bg-[#FFFDFB] rounded-lg">
-      <p className="text-[#3A290D] text-[1rem] font-gabia ">{role}</p>
+    <div className="flex h-7.75 items-center justify-center bg-[#FFFDFB] rounded-lg min-w-14 cursor-pointer">
+      <p className="text-[#3A290D] text-[1rem] font-gabia pl-1 pr-1 ">{role}</p>
     </div>
   );
 };
