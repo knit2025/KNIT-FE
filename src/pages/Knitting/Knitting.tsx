8e259ce60b49f2894fb73894c95a934cc620f2d5
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-12 08:57:09 +0900

    knitting 이미지 1초마다 바뀌도록 설정

--- src/pages/Knitting/Knitting.tsx
+++ src/pages/Knitting/Knitting.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import knitting1 from "../../assets/knitting1.png";
+import knitting2 from "../../assets/knitting2.png";
+import knitting3 from "../../assets/knitting3.png";
+import { useEffect, useState } from "react";
 
 const Knitting: React.FC = () => {
+  const images = [knitting1, knitting2, knitting3];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrentIndex((i) => (i + 1) % images.length);
+    }, 1000);
+    return () => clearInterval(id);
+  }, [images.length]);
+
   return (
     <div className="w-[390px] h-screen flex flex-col justify-center items-center bg-linear-to-b from-[#FFEADB] to-[#FFFFFF]">
       <p className="font-roundfix text-[#826F5F] text-[1.875rem] pb-10 ">
         {"< knitting >"}
       </p>
-      <img src={knitting1} className="w-[22.49388rem] h-[31.37693rem]"></img>
+      <img
+        src={images[currentIndex]}
+        className="w-[20.73594rem] h-[27.98906rem]"
+      ></img>
     </div>
   );
 };
