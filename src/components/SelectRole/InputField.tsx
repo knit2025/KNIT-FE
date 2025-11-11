59c69e6825763a57b5cd7662bdbe95a9defcf394
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-11 11:40:31 +0900

    입력 박스 배경색 다르게 부여

--- src/components/SelectRole/InputField.tsx
+++ src/components/SelectRole/InputField.tsx
@@ -3,14 +3,17 @@ import React from "react";
 interface labelProps {
   label: string;
   placeholder: string;
+  bgColor?: string;
 }
 
-const InputField: React.FC<labelProps> = ({ label, placeholder }) => {
+const InputField: React.FC<labelProps> = ({ label, placeholder, bgColor }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4">
       <p className="text-[#3A290D] text-[0.875rem] font-gabia">{label}</p>
       <input
-        className="w-78 h-10.75 rounded-[0.9375rem] text-[#846F5E] text-[0.75rem] font-gabia pl-4 "
+        className={`w-78 h-10.75 rounded-[0.9375rem] focus:outline-none focus:ring-0 placeholder:text-[#846F5E] text-[#846F5E] text-[0.75rem] font-gabia pl-4  ${
+          bgColor ?? "bg-white"
+        }`}
         placeholder={placeholder}
       ></input>
     </div>
