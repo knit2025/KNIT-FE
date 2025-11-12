bdaa93046684682e3ca4fc593c82a99da0b0ca9d
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-12 09:19:59 +0900

    친절 박스 css 적용

--- src/components/TodayKindness/KindnessBox.tsx
+++ src/components/TodayKindness/KindnessBox.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const KindnessBox: React.FC = () => {
-  return <div></div>;
+type BoxProps = {
+  family: string;
+  content: string;
+};
+
+const KindnessBox: React.FC<BoxProps> = ({ family, content }) => {
+  return (
+    <div className="flex w-85.5 h-30.5 rounded-[1.5625rem]bg-[#FEF1E8] flex-col p-4 gap-2">
+      <p className="text-[#3A290D] font-sans text-[0.8125rem]">{family}</p>
+      <p className="text-[#000000] font-sans text-[0.6875rem]">{content}</p>
+    </div>
+  );
 };
 
 export default KindnessBox;
