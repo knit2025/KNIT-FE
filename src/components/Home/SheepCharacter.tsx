40218a5387ab371206fbc35c9110fc6c37d281af
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-12 13:08:24 +0900

    양 캐릭터 컴포넌트화

---
+++ src/components/Home/SheepCharacter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// import sheep1 from "../../assets/sheep1.png";
+// import sheep2 from "../../assets/sheep2.png";
+// import sheep3 from "../../assets/sheep3.png";
+// import sheep4 from "../../assets/sheep4.png";
+// import sheep5 from "../../assets/sheep5.png";
+// import sheep6 from "../../assets/sheep6.png";
+// import sheep7 from "../../assets/sheep7.png";
+// import sheep8 from "../../assets/sheep8.png";
+// import sheep9 from "../../assets/sheep9.png";
+// import sheep10 from "../../assets/sheep10.png";
+
+const SheepCharacter: React.FC = () => {
+  return <div></div>;
+};
+
+export default SheepCharacter;
