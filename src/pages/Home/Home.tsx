849afb5be51177c30b59d7ee99b9355e06412852
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-12 13:09:34 +0900

    ProgressBar.tsx, ItemCarousel.tsx 컴포넌트 불러오기

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,10 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import messageImg from "../../assets/message.png";
 import sheep1 from "../../assets/sheep1.png";
-import knitBall from "../../assets/knit-ball.png";
-import KnitCarousel from "../../components/Home/KnitCarousel";
+import ItemCarousel from "../../components/Home/ItemCarousel";
+import ProgressBar from "../../components/Home/ProgressBar";
 
 const Home: React.FC = () => {
-  const images = Array.from({ length: 10 }, () => knitBall);
-
   return (
     <>
       <div className="w-[390px] h-screen flex flex-col justify-center items-center bg-linear-to-b from-[#FFFFFF] to-[#DBBBA4]">
@@ -19,10 +17,7 @@ const Home: React.FC = () => {
             topLoc="top-[-70px]"
             leftLoc="left-0"
           />
-          <div className="flex flex-col pl-8 text-left gap-4 justify-center pb-10 font-gabia">
-            <p>스웨터 완성까지</p>
-            <div className="w-80 h-3 rounded-2xl bg-white"></div>
-          </div>
+          <ProgressBar item="니트" />
         </div>
         <div className="flex flex-col items-center">
           <div className="flex flex-col">
@@ -36,13 +31,9 @@ const Home: React.FC = () => {
         <div className="flex pt-20">
           <img className="w-[13.79544rem]" src={sheep1}></img>
         </div>
-        {/* <div className="flex gap-5 pt-10 cursor-pointer pb-30">
-          <img className="w-[3.61913rem] h-[3.5625rem]" src={knitBall}></img>
-          <img className="w-[3.61913rem] h-[3.5625rem]" src={knitBall}></img>
-          <img className="w-[3.61913rem] h-[3.5625rem]" src={knitBall}></img>
-          <img className="w-[3.61913rem] h-[3.5625rem]" src={knitBall}></img>
-        </div> */}
-        <KnitCarousel images={images} />
+        <div className="flex gap-5 pt-10 cursor-pointer pb-30">
+          <ItemCarousel />
+        </div>
 
         <Footer />
       </div>
