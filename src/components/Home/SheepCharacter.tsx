1492b83a6b1e902b957047f29c2f37f5a8b0fcd3
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-13 20:31:28 +0900

    level에 따라 sheep1~sheep11이미지를 자동으로 변경

--- src/components/Home/SheepCharacter.tsx
+++ src/components/Home/SheepCharacter.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 
-// import sheep1 from "../../assets/sheep1.png";
-// import sheep2 from "../../assets/sheep2.png";
-// import sheep3 from "../../assets/sheep3.png";
-// import sheep4 from "../../assets/sheep4.png";
-// import sheep5 from "../../assets/sheep5.png";
-// import sheep6 from "../../assets/sheep6.png";
-// import sheep7 from "../../assets/sheep7.png";
-// import sheep8 from "../../assets/sheep8.png";
-// import sheep9 from "../../assets/sheep9.png";
-// import sheep10 from "../../assets/sheep10.png";
+import sheep1 from "../../assets/sheep1.png";
+import sheep2 from "../../assets/sheep2.png";
+import sheep3 from "../../assets/sheep3.png";
+import sheep4 from "../../assets/sheep4.png";
+import sheep5 from "../../assets/sheep5.png";
+import sheep6 from "../../assets/sheep6.png";
+import sheep7 from "../../assets/sheep7.png";
+import sheep8 from "../../assets/sheep8.png";
+import sheep9 from "../../assets/sheep9.png";
+import sheep10 from "../../assets/sheep10.png";
+import sheep11 from "../../assets/sheep11.png";
 
-const SheepCharacter: React.FC = () => {
-  return <div></div>;
+type SheepProps = {
+  level: number;
+};
+
+const sheepImages = [
+  sheep1,
+  sheep2,
+  sheep3,
+  sheep4,
+  sheep5,
+  sheep6,
+  sheep7,
+  sheep8,
+  sheep9,
+  sheep10,
+  sheep11,
+];
+
+const SheepCharacter: React.FC<SheepProps> = ({ level }) => {
+  const index = Math.min(Math.max(level - 1, 0), sheepImages.length - 1);
+  const img = sheepImages[index];
+  return (
+    <div className="w-[12.49263rem] h-[14.925rem]">
+      <img className="" src={img} alt={`sheep-${level}`}></img>
+    </div>
+  );
 };
 
 export default SheepCharacter;
