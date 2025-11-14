54460e7e03f54571f04e0958647343019fda510f
chootkuri <doyadoya@sookmyung.ac.kr>
2025-11-14 10:15:44 +0900

    로그인 API 연동

--- src/components/Login/Button.tsx
+++ src/components/Login/Button.tsx
@@ -2,14 +2,16 @@ import "../../styles/Global.css";
 
 interface ButtonProps {
   buttonName: string;
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ buttonName }) => {
+const Button: React.FC<ButtonProps> = ({ buttonName, onClick }) => {
   return (
     <div>
       <button
         type="submit"
         className="w-81 h-11.75 rounded-[1.4375rem] bg-[#F7F7F7] text-[#3A290D] font-suit font-weight-500 font-size-1.125rem cursor-pointer"
+        onClick={onClick}
       >
         {buttonName}
       </button>
--- src/components/Login/InputField.tsx
+++ src/components/Login/InputField.tsx
@@ -1,15 +1,24 @@
 interface FieldNameProps {
   fieldName: string;
   type: string;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField: React.FC<FieldNameProps> = ({ fieldName, type }) => {
+const InputField: React.FC<FieldNameProps> = ({
+  fieldName,
+  type,
+  value,
+  onChange,
+}) => {
   return (
     <div>
       <input
         type={type}
         className="w-81 h-9.75 bg-[#F7F7F7] rounded-[0.9375rem] focus:outline-none focus:ring-0 placeholder:text-[#786B56] placeholder: text-[0.875rem] text-[#786B56] pl-6"
         placeholder={fieldName}
+        value={value}
+        onChange={onChange}
       ></input>
     </div>
   );
